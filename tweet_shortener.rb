7de209e5b6745cfbe61a6385923e42e59d2828f0
Victoria Rathsmill <vlrathsmill@gmail.com>
2017-01-15 02:24:03 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "to, two, too" => '2',
+    "for, four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
